aa763dcdf87a8305fad40b03bb17a1fe39b8d96c
k-nasa <htilcs1115@gmail.com>
2019-09-04 12:48:39 +0900

    $yarn upgrade

---
+++ src/Components/Chapter2.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default ({ nextChapter }) => (
+  <div className="chapter2">
+      <p>chapter2</p>
+  </div>
+);
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useEnhance } from "./enhance";
 
 import Chapter1 from "Components/Chapter1";
+import Chapter2 from "Components/Chapter2";
 
 export default () => {
   const { chapter, setChapter } = useEnhance();
@@ -9,6 +10,8 @@ export default () => {
   switch (chapter) {
     case 1:
       return <Chapter1 nextChapter={() => setChapter(2)} />;
+    case 2:
+      return <Chapter2 nextChapter={() => setChapter(3)} />;
     default:
       return <p>fin</p>;
   }
