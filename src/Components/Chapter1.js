4b6070bfa87d614d86f7b7c130115960ece3d60d
k-nasa <htilcs1115@gmail.com>
2019-09-04 12:27:12 +0900

    Add load view

--- src/Components/Chapter1.js
+++ src/Components/Chapter1.js
@@ -2,15 +2,29 @@ import React from "react";
 import LoadIcon from "Assets/syougikoma.png";
 
 export default ({ nextChapter }) => (
-  <div className="chapter1" onAnimationEnd={() => nextChapter()}>
-    <div class="chapter1__bg">
-      <div class="inner">
-        <div />
-        <div />
-        <div />
-        <div />
-      </div>
+  <div className="chapter1">
+    <Bg />
+    <LoadView />
+    <div className="end" onAnimationEnd={() => nextChapter()} />
+  </div>
+);
+
+const Bg = () => (
+  <div className="chapter1__bg">
+    <div className="inner">
+      <div />
+      <div />
+      <div />
+      <div />
     </div>
-    <img src={LoadIcon} alt="load icon" />
+  </div>
+);
+
+const LoadView = () => (
+  <div className="chapter1__load-view">
+    <img className="load-icon" src={LoadIcon} alt="load icon" />
+    <div className="progress-bar">
+      <div />
+    </div>
   </div>
 );
