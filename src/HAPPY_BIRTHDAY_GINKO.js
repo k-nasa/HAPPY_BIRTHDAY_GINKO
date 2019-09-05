28ccb34a6df8a2ffb9902dcc96b340f0f2534879
k-nasa <htilcs1115@gmail.com>
2019-09-05 20:00:57 +0900

    update

--- src/HAPPY_BIRTHDAY_GINKO.js
+++ src/HAPPY_BIRTHDAY_GINKO.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "styles/style.scss";
 import Home from "./Pages/Home";
 
 const About = () => <p>このサイトについて</p>;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HAPPY_BIRTHDAY_GINKO from "HAPPY_BIRTHDAY_GINKO";
 import * as serviceWorker from "./serviceWorker";
+import "styles/style.scss";
 import { Provider } from "react-redux";
 import configureStore from "store";
 
