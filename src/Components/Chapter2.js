4438598ea378b3e9a345ec4e07942b817a8c2251
k-nasa <htilcs1115@gmail.com>
2019-09-05 19:48:00 +0900

    fix button

--- src/Components/Chapter2.js
+++ src/Components/Chapter2.js
@@ -19,7 +19,7 @@ const ShareBtn = () => (
       target="_blank"
       href="https://twitter.com/intent/tweet?url=https://happy-birthday-ginko.netlify.com&text=%E7%A9%BA%E9%8A%80%E5%AD%90%E8%AA%95%E7%94%9F%E7%A5%AD2019%20%E9%9D%9E%E5%85%AC%E5%BC%8F%E3%82%B5%E3%82%A4%E3%83%88%20/%20@nasa_desu"
     >
-        Twitterでシェア
+      Twitterでシェア
     </a>
   </div>
 );
